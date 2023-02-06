fc88b1afa8c766d0610f62e28c450d62aa2455a5
Diogo Martins <a22027@d1-08-11.dped.local>
2023-02-06 16:30:08 +0000

    criação de janela

--- src/main/java/com/mycompany/projetogithub/ProjetoGitHub.java
+++ src/main/java/com/mycompany/projetogithub/ProjetoGitHub.java
@@ -14,5 +14,6 @@ public class ProjetoGitHub {
     public static void main(String[] args) {
         System.out.println("olá mundo!");
         // falta um comentário....
+        System.out.println("Fim");
     }
 }
